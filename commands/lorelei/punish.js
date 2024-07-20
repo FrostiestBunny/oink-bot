0ed57b85c10c1b42ba9285d6dc73be209c70c580
Frost Bunny <acezackfair@gmail.com>
2024-07-20 20:29:07 +0200

    Add punish extreme

--- commands/lorelei/punish.js
+++ commands/lorelei/punish.js
@@ -1,4 +1,12 @@
-const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const {
+  SlashCommandBuilder,
+  PermissionFlagsBits,
+  ButtonBuilder,
+  ButtonStyle,
+  ActionRowBuilder,
+  ActionRow,
+  ComponentType,
+} = require('discord.js');
 
 const data = new SlashCommandBuilder()
   .setName('punish')
@@ -48,10 +56,108 @@ const data = new SlashCommandBuilder()
       .addUserOption((option) =>
         option.setName('target5').setDescription('The evildoer to punish')
       )
+  )
+  .addSubcommand((subcommand) =>
+    subcommand
+      .setName('extreme')
+      .setDescription('Only for the worst offenders, use with care.')
+      .addUserOption((option) =>
+        option
+          .setName('target')
+          .setDescription('The evil person to punish')
+          .setRequired(true)
+      )
+      .addIntegerOption((option) =>
+        option
+          .setName('duration')
+          .setDescription('Duration of the timeout in seconds')
+      )
   );
 
+const extremePunish = async (interaction, target, duration) => {
+  const gomen =
+    'Gomenasorry ojousama supreme commander cult leader hime princess nya nya';
+
+  let letters = gomen.split('');
+  letters = [...new Set(letters)];
+  const buttons = [];
+
+  for (let i = 0; i < letters.length; i++) {
+    let letter = letters[i];
+
+    if (letter === ' ') letter = 'space';
+
+    const btn = new ButtonBuilder()
+      .setCustomId(letter)
+      .setLabel(letter.toUpperCase())
+      .setStyle(ButtonStyle.Secondary);
+    buttons.push(btn);
+  }
+
+  const rows = [];
+
+  const chunkSize = 5;
+  for (let i = 0; i < buttons.length; i += chunkSize) {
+    const chunk = buttons.slice(i, i + chunkSize);
+    const row = new ActionRowBuilder().addComponents(...chunk);
+    rows.push(row);
+  }
+
+  const response = await interaction.channel.send({
+    content:
+      'Use the buttons to type out the full gomenasorry message. Good luck! <:nyaSalute:1251618350736478270>',
+    components: [...rows],
+  });
+
+  const filter = (i) => i.user.id === target.id;
+
+  const collector = response.createMessageComponentCollector({
+    componentType: ComponentType.Button,
+    time: 3 * 60 * 1000,
+    filter: filter,
+  });
+
+  let text = '';
+
+  collector.on('collect', async (i) => {
+    text += i.customId === 'space' ? ' ' : i.customId;
+    await i.update(text);
+
+    if (!gomen.startsWith(text)) {
+      collector.stop('failure');
+    }
+
+    if (text === gomen) {
+      collector.stop('success');
+      await interaction.channel.send(
+        'Congratulations, you live to see another day.'
+      );
+    }
+  });
+
+  collector.on('ignore', async (i) => {
+    // when an interaction is ignored
+  });
+
+  collector.on('end', async (collected, reason) => {
+    // when the timer runs out
+    if (reason !== 'success') {
+      if (reason === 'failure') {
+        await response.reply('You messed up, delete yourself.');
+      } else {
+        await response.reply('You were too late, delete yourself.');
+      }
+
+      if (target.manageable || target.moderatable) {
+        await target.timeout(duration, 'Failed to apologize');
+      }
+    }
+  });
+};
+
 const execute = async (interaction) => {
-  const faslaId = '597852157827612673';
+  const cathId = '133956553505112064';
+
   const gomen =
     'Gomenasorry ojousama supreme commander cult leader hime princess nya nya';
   const timeout_duration =
@@ -77,9 +183,9 @@ const execute = async (interaction) => {
       }
     });
 
-    // always add fasla to the target list as per commander's instructions
-    const fasla = await interaction.guild.members.fetch(faslaId);
-    validTargets.push(fasla);
+    // always add cath to the target list as per commander's instructions
+    const cath = await interaction.guild.members.fetch(cathId);
+    validTargets.push(cath);
 
     punishMessage += validTargets.join(', ');
     punishMessage +=
@@ -108,7 +214,6 @@ const execute = async (interaction) => {
       .then(async (collected) => {
         collected.each(async (msg) => {
           repliedMap[msg.member.id] = true;
-          console.log(msg);
           if (msg.content.toLowerCase() === gomen.toLowerCase()) {
             await msg.reply(`You're off the hook for now, oinker.`);
           } else {
@@ -187,6 +292,13 @@ const execute = async (interaction) => {
       });
 
     await interaction.editReply("It's done, boss.");
+  } else if (interaction.options.getSubcommand() == 'extreme') {
+    await interaction.deferReply({ ephemeral: true });
+    const target = interaction.options.getMember('target');
+    await extremePunish(interaction, target, timeout_duration);
+    await interaction.editReply(
+      '<:nyaSalute:1251618350736478270> yes supreme commyander princess hime nya nya'
+    );
   }
 };
 
