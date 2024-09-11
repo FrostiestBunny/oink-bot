96cbaeb63a65d50e8202a48779cd2e25c665992b
Frost Bunny <acezackfair@gmail.com>
2024-09-11 16:01:39 +0200

    Fix formatting

--- commands/lorelei/blackjack.js
+++ commands/lorelei/blackjack.js
@@ -8,29 +8,29 @@ const {
   ButtonStyle,
   ActionRowBuilder,
   ComponentType,
-} = require("discord.js");
+} = require('discord.js');
 
 //name of slash command & description
 const data = new SlashCommandBuilder()
-  .setName("blackjack")
-  .setDescription("Play a game of blackjack");
+  .setName('blackjack')
+  .setDescription('Play a game of blackjack');
 
 //instantiate arrays
-const suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
+const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
 const values = [
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "Jack",
-  "Queen",
-  "King",
-  "Ace",
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '10',
+  'Jack',
+  'Queen',
+  'King',
+  'Ace',
 ];
 
 //create the deck
@@ -58,9 +58,9 @@ const handValue = (hand) => {
   let value = 0;
   let aceCount = 0;
   for (let card of hand) {
-    if (["Jack", "Queen", "King"].includes(card.value)) {
+    if (['Jack', 'Queen', 'King'].includes(card.value)) {
       value += 10;
-    } else if (card.value === "Ace") {
+    } else if (card.value === 'Ace') {
       aceCount++;
       value += 11;
     } else {
@@ -77,7 +77,7 @@ const handValue = (hand) => {
 
 //start the game
 const execute = async (interaction) => {
-  await interaction.reply("Game is starting right nyaow!");
+  await interaction.reply('Game is starting right nyaow!');
 
   //declare variables
   let deck = shuffleDeck(createDeck());
@@ -90,21 +90,21 @@ const execute = async (interaction) => {
 
   //buttons
   const btnHit = new ButtonBuilder()
-    .setCustomId("hit")
-    .setLabel("Hit")
+    .setCustomId('hit')
+    .setLabel('Hit')
     .setStyle(ButtonStyle.Secondary);
 
   const btnStand = new ButtonBuilder()
-    .setCustomId("stand")
-    .setLabel("Stand")
+    .setCustomId('stand')
+    .setLabel('Stand')
     .setStyle(ButtonStyle.Secondary);
 
   const row = new ActionRowBuilder().addComponents(btnHit, btnStand);
 
   //game embed
   let embed = new EmbedBuilder()
-    .setColor("LuminousVividPink")
-    .setTitle("Blackjack Table")
+    .setColor('LuminousVividPink')
+    .setTitle('Blackjack Table')
     .addFields({
       name: `${playerName}'s hand:`,
       value: `${handToString(playerHand)} (Value: ${handValue(playerHand)})`,
@@ -121,15 +121,15 @@ const execute = async (interaction) => {
   });
 
   const filter = (i) =>
-    i.customId === "hit" ||
-    (i.customId === "stand" && i.user.id === interaction.member.id);
+    i.customId === 'hit' ||
+    (i.customId === 'stand' && i.user.id === interaction.member.id);
 
   //repeat until game is over
   while (!gameOver) {
     //display hands, 1 of OinkBot's cards are hidden
     embed = new EmbedBuilder()
-      .setColor("LuminousVividPink")
-      .setTitle("Blackjack Table")
+      .setColor('LuminousVividPink')
+      .setTitle('Blackjack Table')
       .addFields({
         name: `${playerName}'s hand:`,
         value: `${handToString(playerHand)} (Value: ${handValue(playerHand)})`,
@@ -150,9 +150,9 @@ const execute = async (interaction) => {
 
     //hit or stand
     if (!playerStand) {
-      let action = prompt("Do you want to (h)it or (s)tand?");
+      let action = prompt('Do you want to (h)it or (s)tand?');
       //hit
-      if (action === "h") {
+      if (action === 'h') {
         playerHand.push(deck.pop());
         if (handValue(playerHand) > 21) {
           console.log(
@@ -164,7 +164,7 @@ const execute = async (interaction) => {
           gameOver = true;
         }
         //stand
-      } else if (action === "s") {
+      } else if (action === 's') {
         playerStand = true;
       }
     } else {
@@ -189,12 +189,12 @@ const execute = async (interaction) => {
     }
   }
   //finish output
-  await interaction.followUp("Game over.");
+  await interaction.followUp('Game over.');
 };
 
 //toString for displaying card hands
 const handToString = (hand) => {
-  return hand.map((card) => `${card.value} of ${card.suit}`).join(", ");
+  return hand.map((card) => `${card.value} of ${card.suit}`).join(', ');
 };
 
 module.exports = { data, execute };
