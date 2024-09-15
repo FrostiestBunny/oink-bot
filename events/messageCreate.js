3ced56b8930ed15c76e268809e5f482a331932ba
FoolOfMadness <jim.bool@outlook.com>
2024-09-15 23:49:23 +0100

    autopublish announcements

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -9,23 +9,34 @@ const { extremePunish } = require('../commands/lorelei/punish.js');
 module.exports = {
   name: Events.MessageCreate,
   /**
-   * @param {Message} message 
+   * @param {Message} message
    */
   async execute(message) {
-    // 60s
-    const PUNISH_DURATION = 60 * 1000;
-    if (message.member.id == process.env.CLIENT_ID) return;
+    try {
+      //check if message, member, or bannedWords are invalid, or if the message is from oinkbot
+      if (
+        !message?.member ||
+        message.member.id === process.env.CLIENT_ID ||
+        !message.client.bannedWords
+      )
+        return;
 
-    for (const element of message.client.bannedWords) {
-      const word = Object.values(element)[0];
-      if (word.test(message.content)) {
-        await extremePunish(
-          message.channel,
-          message.member,
-          PUNISH_DURATION,
-          150
-        );
+      //60s
+      const PUNISH_DURATION = 60 * 1000;
+
+      for (const element of message.client.bannedWords) {
+        const word = Object.values(element)[0];
+        if (word.test(message.content)) {
+          await extremePunish(
+            message.channel,
+            message.member,
+            PUNISH_DURATION,
+            150
+          );
+        }
       }
+    } catch (error) {
+      console.error('Error in messageCreate event:', error);
     }
   },
 };
