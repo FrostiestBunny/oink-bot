cda0145de4b8f4a3cb0fe388ff56db8d64af6e61
Jim Bool <jim.bool@outlook.com>
2024-09-17 11:34:13 +0000

    diceroll overhaul

--- commands/lorelei/diceroll.js
+++ commands/lorelei/diceroll.js
@@ -4,62 +4,39 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 //name of slash command & description
 const data = new SlashCommandBuilder()
   .setName('diceroll')
-  .setDescription('Roll specified sided dice')
-  .addSubcommand((subcommand) =>
-    subcommand
-      .setName('basic')
-      .setDescription('Roll specified sided dice without a modifier')
-      .addIntegerOption((option) =>
-        option
-          .setName('sides')
-          .setDescription('Number of sides on the die (maximum 100)')
-          .setRequired(true)
-      )
-      .addIntegerOption((option) =>
-        option
-          .setName('quantity')
-          .setDescription('Number of dice to roll (maximum 100)')
-          .setRequired(true)
-      )
+  .setDescription('Roll some dice')
+  .addIntegerOption((option) =>
+    option
+      .setName('sides')
+      .setDescription('Number of sides on the die (default 20, maximum 100)')
+      .setRequired(false)
   )
-  .addSubcommand((subcommand) =>
-    subcommand
-      .setName('modified')
-      .setDescription('Roll specified sided dice with a modifier')
-      .addIntegerOption((option) =>
-        option
-          .setName('sides')
-          .setDescription('Number of sides on the die (maximum 100)')
-          .setRequired(true)
+  .addIntegerOption((option) =>
+    option
+      .setName('quantity')
+      .setDescription('Number of dice to roll (default 1, maximum 100)')
+      .setRequired(false)
+  )
+  .addIntegerOption((option) =>
+    option
+      .setName('modifier')
+      .setDescription(
+        'Modifier to add or subtract from the roll (default 0, between -50 and +50)'
       )
-      .addIntegerOption((option) =>
-        option
-          .setName('quantity')
-          .setDescription('Number of dice to roll (maximum 100)')
-          .setRequired(true)
-      )
-      .addIntegerOption((option) =>
-        option
-          .setName('modifier')
-          .setDescription(
-            'Modifier to add or subtract from the roll (between -50 and +50)'
-          )
-          .setRequired(true)
-      )
+      .setRequired(false)
   );
 
 //roll the dice
 const execute = async (interaction) => {
   try {
-    //set playername as username
+    //set player name as username
     const playerName = interaction.member.displayName;
-    //get subcommand
-    const subcommand = interaction.options.getSubcommand() || 'basic'; //default basic
     //get variables
     const sides = interaction.options.getInteger('sides') || 20; //default 20
     const quantity = interaction.options.getInteger('quantity') || 1; //default 1
+    const modifier = interaction.options.getInteger('modifier') || 0; //default 0
 
-    //get diceroll result
+    //get dice roll result
     const rollDie = (sides) => {
       return Math.floor(Math.random() * sides) + 1;
     };
@@ -80,33 +57,22 @@ const execute = async (interaction) => {
       return;
     }
 
+    //modifier must be between -50 and +50
+    if (modifier < -50 || modifier > 50) {
+      await interaction.reply(
+        'Please enter a modifier between -50 and +50! <:nyaAngry:1251302942456414218>'
+      );
+      return;
+    }
+
     //instantiate array
     let results = [];
 
-    //basic roll (no modifier)
-    if (subcommand === 'basic') {
-      for (let i = 0; i < quantity; i++) {
-        results.push(rollDie(sides));
-      }
-    }
-
-    //roll with modifier
-    else if (subcommand === 'modified') {
-      const modifier = interaction.options.getInteger('modifier');
-
-      //modifier must be between +-50
-      if (modifier < -50 || modifier > 50) {
-        await interaction.reply(
-          'Please enter a modifier between -50 and +50! <:nyaAngry:1251302942456414218>'
-        );
-        return;
-      }
-
-      for (let i = 0; i < quantity; i++) {
-        const roll = rollDie(sides);
-        const modifiedRoll = roll + modifier;
-        results.push(modifiedRoll);
-      }
+    //roll the dice
+    for (let i = 0; i < quantity; i++) {
+      const roll = rollDie(sides);
+      const modifiedRoll = roll + modifier;
+      results.push(modifiedRoll);
     }
 
     //make an embed with the results
@@ -115,10 +81,58 @@ const execute = async (interaction) => {
       .setTitle('Dice Roll Results')
       .setThumbnail(interaction.member.displayAvatarURL())
       .setDescription(
-        `${playerName} rolled ${quantity} ${sides}-sided dice and got:\n**${results.join(
-          ', '
-        )}**`
+        `${playerName} rolled ${quantity} ${sides}-sided dice${
+          modifier !== 0
+            ? ` with a modifier of ${modifier >= 0 ? '+' : ''}${modifier}`
+            : ''
+        } and got:\n**${results.join(', ')}**`
       );
+
+    //calculate sum & averages if rolling multiple dice
+    if (quantity > 1) {
+      //calc sum
+      const sum = results.reduce((a, b) => a + b, 0);
+      //calc average
+      const mean = (sum / results.length).toFixed(2);
+
+      //sort array to get the median value
+      const sortedResults = [...results].sort((a, b) => a - b);
+      const median =
+        sortedResults.length % 2 === 0
+          ? (
+              (sortedResults[sortedResults.length / 2 - 1] +
+                sortedResults[sortedResults.length / 2]) /
+              2
+            ).toFixed(2)
+          : sortedResults[Math.floor(sortedResults.length / 2)];
+
+      //get mode
+      const mode = (() => {
+        const frequency = {};
+        let maxFreq = 0;
+        let modes = [];
+
+        //loop to check each value of result & store how frequently the value appears
+        for (const num of results) {
+          frequency[num] = (frequency[num] || 0) + 1;
+          if (frequency[num] > maxFreq) {
+            maxFreq = frequency[num];
+            modes = [num];
+          } else if (frequency[num] === maxFreq) {
+            modes.push(num);
+          }
+        }
+        //if results are equally frequent there is no mode value
+        return modes.length === results.length ? 'No mode' : modes.join(', ');
+      })();
+
+      //append nyaNerd stats to the embed
+      embed.addFields({
+        name: 'Overall Results <:nyaNerd:1251606395523039303>',
+        value: `Mean: ${mean}, Median: ${median}, Mode: ${mode}, Sum: ${sum}`,
+        inline: true,
+      });
+    }
 
     //print results
     await interaction.reply({ embeds: [embed] });
