1933922f1465ab36806fb7d0780440baa315012d
Jim Bool <jim.bool@outlook.com>
2024-11-29 09:57:08 +0000

    magic8ball but better and no bully

--- commands/lorelei/magic8ball.js
+++ commands/lorelei/magic8ball.js
@@ -12,57 +12,42 @@ const data = new SlashCommandBuilder()
       .setRequired(true)
   );
 
-//responses, 2D array to seperate bully responses from normal ones
-const responses = {
-  normal: [
-    'Yes!',
-    'No!',
-    'Maybe? Maybe not...?',
-    "I don't know!!",
-    "Can you repeat the question? I can't hear you",
-    "You're not the boss of me!!",
-    'Sure.',
-    'As I see it, yes.',
-    'Most likely.',
-    'Outlook good.',
-    'Signs point to yes.',
-    "I'm getting sleepy, try again.",
-    'Ask again later.',
-    'Better not tell you now.',
-    'Cannot predict now.',
-    'Concentrate and ask again.',
-    "Don't count on it, buddy.",
-    'My reply is no and will forever be no.',
-    'My sources say no.',
-    'Outlook not so good.',
-    'Very doubtful.',
-    "It's nyaover.",
-    'It could work, who knows.',
-    'Congratulations!',
-    'Stop shaking me!',
-    "I'm sorry, but I cannot fulfill this request as it goes against LorelAI use policy.",
-    'Look behind you.',
-    "OMG that's crazy!",
-    'All signs point to skibidi.',
-    'Okay buddy.',
-    "I don't think so pal.",
-    'You will explode.',
-    'CORN DOGS!',
-    'Uppies for you!',
-  ],
-  bully: [
-    'UGHH',
-    'nyaFuck you.',
-    'I hate you guys.',
-    'You guys should just die!',
-    "I'm gonna kill you.",
-    "Why don't you just give up and die!",
-    'Why are you still holding on? Just kill yourself!',
-    "You're so dead, you are actually deceased right nyaow.",
-    "I'm gonna find you, I'm gonna hunt you down, & I'm gonna kill you.",
-    "I am currently operating on the most recent patch. Please read the patch notes for the developer's comments on this. A common complaint users like you have submitted can be easily corrected by ending yourself.",
-  ],
-};
+//responses
+const responses = [
+  'Yes!',
+  'No!',
+  'Maybe? Maybe not...?',
+  "I don't know!!",
+  "Can you repeat the question? I can't hear you",
+  "You're not the boss of me!!",
+  'Sure.',
+  'As I see it, yes.',
+  'Most likely.',
+  'Outlook good.',
+  'Signs point to yes.',
+  "I'm getting sleepy, try again.",
+  'Ask again later.',
+  'Better not tell you now.',
+  'Cannot predict now.',
+  'Concentrate and ask again.',
+  "Don't count on it, buddy.",
+  'My reply is no and will forever be no.',
+  'My sources say no.',
+  'Outlook not so good.',
+  'Very doubtful.',
+  "It's nyaover.",
+  'It could work, who knows.',
+  'Congratulations!',
+  'Stop shaking me!',
+  "I'm sorry, but I cannot fulfill this request as it goes against LorelAI use policy.",
+  'Look behind you.',
+  "OMG that's crazy!",
+  'All signs point to skibidi.',
+  'Okay buddy.',
+  "I don't think so pal.",
+  'CORN DOGS!',
+  'Uppies for you!',
+];
 
 //ask the 8ball
 const execute = async (interaction) => {
@@ -70,23 +55,8 @@ const execute = async (interaction) => {
     //get the question from the user
     const question = interaction.options.getString('question');
 
-    //pig-pen/bot-spam & oinker bully check
-    const channel = interaction.channel.id;
-    let availableResponses = [];
-    if (
-      interaction.member.roles.cache.has('1256247572582891542') &&
-      (channel === '1247276527125794867' || channel === '1283447730693345353')
-    ) {
-      //use all responses in pig-pen & bot-spam only if user is oinker
-      availableResponses = [...responses.normal, ...responses.bully];
-    } else {
-      //use only normal responses any other case
-      availableResponses = responses.normal;
-    }
-
     //get a random response
-    const response =
-      availableResponses[Math.floor(Math.random() * availableResponses.length)];
+    const response = responses[Math.floor(Math.random() * responses.length)];
 
     //make an embed with the response
     const embed = new EmbedBuilder()
